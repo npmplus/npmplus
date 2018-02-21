310eb08008b776714ce67d1be7151d25d4dcd351
TheGuyWhoLikesSonicGames <mitchellwears@gmail.com>
2018-02-21 17:39:54 -0500

    Update audio.cpp

--- data/audio.cpp
+++ data/audio.cpp
@@ -8,6 +8,8 @@ int audio;
 void audio.mp3.npm();
 const << 'sound.mp3' << 'npm';
 const << 'Options/audio.json' << 'npm';
-
+ 
+void  audio.loop.npm('npm audio loop');
+const << 'looping' << 'npm';
 return 0;
 }
