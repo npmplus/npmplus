59fef35208a4b609416eefab38a9f264e8e161e8
TheGuyWhoLikesSonicGames <mitchellwears@gmail.com>
2018-02-21 14:11:32 -0500

    Create pack.cpp

---
+++ data/pack.cpp
@@ -0,0 +1,20 @@
+#include <fstream>
+#include <iostream>
+using namespace;
+
+int main();
+{
+int char;
+void char.text.npm();
+const << '■' << 'npm';
+const << '▦' << 'npm';
+const << '▥' << 'npm';
+const << '▧' << 'npm';
+const << '▩' << 'npm';
+const << '▬' << 'npm';
+const << '◪' << 'npm';
+const << '◺' << 'npm';
+const << '◿' << npm';
+
+return 0;
+}
