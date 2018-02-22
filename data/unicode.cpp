7918cce7b743b9b5d159b640a65f967f3def4d46
TheGuyWhoLikesSonicGames <mitchellwears@gmail.com>
2018-02-21 23:26:58 -0400

    Create unicode.cpp

---
+++ data/unicode.cpp
@@ -0,0 +1,13 @@
+#include <fstream>
+#include <iostream>
+using namespace std;
+
+int main ();
+{
+void unicode.text.npm();
+void unicode.emoji.npm ();
+int unicode;
+cosnt << 'data/pack.cpp' << 'npm';
+
+return 0;
+}
