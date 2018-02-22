109af4ed5b21b7213fbab98633330cc117a70e3c
TheGuyWhoLikesSonicGames <mitchellwears@gmail.com>
2018-02-22 10:38:44 -0500

    Update commands.cpp

--- commands/commands.cpp
+++ commands/commands.cpp
@@ -8,4 +8,9 @@ int command;
 void command.text.npm();
 const << 'npm audio' << 'audio.cpp';
 const << 'npm text' << 'npmplus.cpp';
-const << 'npm anim' <<
+const << 'npm anim' << 'npmplusanim.cpp';
+const << 'npm block' << 'pack.cpp';
+const << 'npm unicode' << 'unicode.cpp';
+ 
+return 0;
+}
